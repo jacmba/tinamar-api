2fa10c481e70d442358286ea6c0fbd6b6acc20e6
Jacinto Mba Cantero <jacmba@gmailcom>
2020-02-24 01:10:26 +0000

    Added CORS header

--- src/api/controller.go
+++ src/api/controller.go
@@ -23,5 +23,6 @@ func league(w http.ResponseWriter, req *http.Request) {
 	}
 
 	w.Header().Add("Content-Type", "application/json")
+	w.Header().Add("Access-Control-Allow-Origin", "*")
 	w.Write(js)
 }
